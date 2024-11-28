b45f7be13f2b11d0014049b2f3b95a03886bdfaf
Andy Luu <andyluu324@gmail.com>
2024-11-27 23:17:41 -0500

    Updated test with chi squared data

--- post-data/Correctness Kruskal Wallis Tests.R
+++ post-data/Correctness Kruskal Wallis Tests.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(dplyr)
 
 myData = valid_correctness_scores
 print(myData)
@@ -15,3 +16,17 @@ print(levels(myData$TIMELINE))
 ggplot(myData, aes(fill=PID, y=SCORE, x=TIMELINE)) + 
   geom_bar(position='stack', stat='identity')
 
+# Select the columns of interest
+selected_data <- myData %>% select(TIMELINE, SCORE)
+
+# Create a contingency table for TIMELINE AND SCORE
+contingency_table <- table(selected_data$SCORE, selected_data$TIMELINE)
+
+# View the contingency table
+print(contingency_table)
+
+# Perform chi-square test
+chi_square_test <- chisq.test(contingency_table)
+
+# View the results
+print(chi_square_test)
